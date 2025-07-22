87cbf4e35f404e60acdeaae52c81e07f0b166c34
Thy_Dye <tiedye3.141@gmail.com>
2025-07-22 12:42:13 -0700

    Sphere formula hittable function
    implemented the function to detect if a sphere is hit from a ray

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,20 @@ void write_color(unsigned char* img, const int &comp, const int &pixel_index, co
             img[pixel_index * comp + 3] = 255;                                          // alpha channel
     }
 
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 oc =center - r.origin();
+    double a = dot(r.direction(), r.direction());
+    double b = -2.0 * dot(r.direction(), oc);
+    double c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0); //discriminant tells us if the point at the sphere exists
+}
+
 color3 ray_color(const ray& r){
+    if(hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color3(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color3(1.0, 1.0, 1.0) + a*color3(0.5, 0.7, 1.0); //lerp
@@ -71,7 +84,7 @@ int main ()
 
 
     //rendering section
-    for (int j = image_h - 1;  j >= 0; j--)
+    for (int j = 0;  j < image_h; j++)
     {
         std::clog << "Scanlines remaining: " << (image_h - j) << " \n" << std::flush;
 
