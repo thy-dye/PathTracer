6983d23aed8ca55cd204792f426ef006ade1a85a
Thy_Dye <tiedye3.141@gmail.com>
2025-07-22 15:50:30 -0700

    ray trace normals of the sphere
    implemented the normals of the sphere

--- src/main.cpp
+++ src/main.cpp
@@ -22,23 +22,30 @@ void write_color(unsigned char* img, const int &comp, const int &pixel_index, co
             img[pixel_index * comp + 3] = 255;                                          // alpha channel
     }
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc =center - r.origin();
     double a = dot(r.direction(), r.direction());
     double b = -2.0 * dot(r.direction(), oc);
     double c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0); //discriminant tells us if the point at the sphere exists
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant))/(2.0 * a);
+    }
 }
 
-color3 ray_color(const ray& r){
-    if(hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color3(1, 0, 0);
+color3 ray_color(const ray& r) {
+    double t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color3(N.x()+1, N.y()+1, N.z()+1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color3(1.0, 1.0, 1.0) + a*color3(0.5, 0.7, 1.0); //lerp
+    double a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color3(1.0, 1.0, 1.0) + a*color3(0.5, 0.7, 1.0);
 }
 
 int main ()
