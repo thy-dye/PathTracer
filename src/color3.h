0e3340add8cc32a1438e19a3decbaeda0a0c6e6d
Thy_Dye <tiedye3.141@gmail.com>
2025-12-08 12:57:15 -0800

    added gamma correction
    interesting to see that we percieve light in a logarithmic function and therefore can use a power function to approximate  how we percieve the light and dark values.

--- src/color3.h
+++ src/color3.h
@@ -6,12 +6,25 @@
 
 using color3 = vec3;
 
+inline double linear_to_gamma(double linear_component){
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+    return 0;
+}
+
 //color the image with given pixel_color
 void write_color(unsigned char* img, const int &comp, const int &pixel_index, const color3 &pixel_color) {
         auto r = pixel_color.r();
         auto g = pixel_color.g();
         auto b = pixel_color.b();
 
+        //turn rgb linear space to gamma space
+        r = linear_to_gamma(r);
+        g = linear_to_gamma(g);
+        b = linear_to_gamma(b);
+
+        //translate [0,1] range of components to [0,255]
         static const interval intensity(0, 0.999);
         img[pixel_index * comp] = int(255.99 * intensity.clamp(r));                    // red channel
         img[pixel_index * comp + 1] = int(255.99 * intensity.clamp(g));                // green channel
